4403cd9731fb99c40c6bcc8fae27c02081c83f8e
demo4him <mac69089@gmail.com>
2026-02-01 11:05:29 +0200

    Add files via upload

---
+++ download Nvidia-Driver.sh
@@ -0,0 +1,75 @@
+#! /bin/bash
+# Autor= João Batista Ribeiro
+# Bugs, Agradecimentos, Críticas "construtivas"
+# me envie um e-mail. Ficarei Grato!
+# e-mail: joao42lbatista@gmail.com
+#
+# Este programa é um software livre; você pode redistribui-lo e/ou
+# modifica-lo dentro dos termos da Licença Pública Geral GNU como
+# publicada pela Fundação do Software Livre (FSF); na versão 2 da
+# Licença, ou (na sua opinião) qualquer versão.
+#
+# Este programa é distribuído na esperança que possa ser útil,
+# mas SEM NENHUMA GARANTIA; sem uma garantia implícita de ADEQUAÇÃO a
+# qualquer MERCADO ou APLICAÇÃO EM PARTICULAR.
+#
+# Veja a Licença Pública Geral GNU para mais detalhes.
+# Você deve ter recebido uma cópia da Licença Pública Geral GNU
+# junto com este programa, se não, escreva para a Fundação do Software
+#
+# Livre(FSF) Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Descrição: Script to download the "last" version of NVIDIA drivers "source"
+#
+# Last update: 23/03/2025
+#
+# https://slackbuilds.org/repository/15.0/system/nvidia-kernel/
+# https://slackbuilds.org/repository/15.0/system/nvidia-driver/ with COMPAT32="yes"
+#
+echo -e "\n# Download source to build nvidia driver #"
+
+versionDl="580.105.08"
+echo "Version: \"$versionDl\""
+linkDl="https://download.nvidia.com/XFree86"
+
+download_x86_64=("$linkDl/Linux-x86_64/$versionDl/NVIDIA-Linux-x86_64-$versionDl.run"
+"$linkDl/nvidia-installer/nvidia-installer-$versionDl.tar.bz2"
+"$linkDl/nvidia-modprobe/nvidia-modprobe-$versionDl.tar.bz2"
+"$linkDl/nvidia-persistenced/nvidia-persistenced-$versionDl.tar.bz2"
+"$linkDl/nvidia-settings/nvidia-settings-$versionDl.tar.bz2"
+"$linkDl/nvidia-xconfig/nvidia-xconfig-$versionDl.tar.bz2")
+
+md5sum_x86_64=("27c62c49264e1a1da92ebcede9cf76bc"
+"3f5dcb9cbb961f4c5f9c270445c7ea58"
+"f0324056d943150717adf2f6e9001124"
+"deb0e40a2674c93d94d8bfb741dc9061"
+"71c86e1fb3d71e412e8b65ebb628b28a"
+"f1c053ead29e1a5afe605cb5f80470cf")
+
+mkdir -p "source_$versionDl/nvidia-driver/nvidia-kernel"
+cd "source_$versionDl/nvidia-driver/" || exit
+
+checksum(){
+    sum=$(md5sum "$1" | cut -d ' ' -f1)
+
+    if [ "$sum" != "$2" ]; then
+        echo -e "\n\tWARNING: checksum failed: $1\n"
+        sleep 3
+    else
+        echo -e "md5sum $1: Ok"
+    fi
+}
+
+len=${#download_x86_64[@]}
+for (( i=0; i < len; i++)); do
+    echo -e "\nwget -c ${download_x86_64[$i]}"
+    wget -qc "${download_x86_64[$i]}"
+
+    #echo -e "\n${download_x86_64[$i]} ${md5sum_x86_64[$i]}"
+    checksum "$(basename "${download_x86_64[$i]}")" "${md5sum_x86_64[$i]}"
+done
+
+# create link
+ln -s $PWD/NVIDIA-Linux-x86_64-$versionDl.run nvidia-kernel/
+
+echo -e "\nDone!\n"
