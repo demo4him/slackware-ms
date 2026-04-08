0e117680c8b631e6809c67d93cb7f78895563af0
demo4him <mac69089@gmail.com>
2026-04-08 10:03:36 +0300

    Add files via upload
    You have to check /dev/sd?1(2,3,4) according to yours hardware. Modify in script! I posted info!

---
+++ move_partition-home_on_other_partition.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#sudo mkdir /mnt/tmp
+sudo mount /dev/sdb1 /mnt/tmp
+sudo rsync -avx /home/ /mnt/tmp
+sudo mount /dev/sdb1 /home
+sudo umount /home
+sudo rm -rf /home/*
+echo "/dev/sdb1        /home            ext4        defaults         1   2" >> /etc/fstab
+echo "Reboot Slackware"
